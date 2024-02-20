03345c26df938efbec3ed8c905d9407a7a035a47
Jean <jeantorresricse@gmail.com>
2024-02-19 21:36:52 -0500

    configuracion del swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api/v1');
+  //Validacion Flobal
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  //Configuracion de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Gestion de recoleccion de basura')
+    .setDescription('Aqui los api listado.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  app.enableCors();
+
+  //Puerto del servidor
+  await app.listen(process.env.PORT_SERVER);
 }
 bootstrap();
