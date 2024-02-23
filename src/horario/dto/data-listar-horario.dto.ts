efc9cf350125f1b8d16083a41ea500abb097e2ea
Jean <jeantorresricse@gmail.com>
2024-02-23 17:22:56 -0500

    Quitando lo comentado de dto del horario

--- src/horario/dto/data-listar-horario.dto.ts
+++ src/horario/dto/data-listar-horario.dto.ts
@@ -4,7 +4,7 @@ import { IsIn, IsNumber, IsNumberString, IsString, isIn } from "class-validator"
 export class DataListarHorarioDto{
     // @Type(()=> Number)
     @IsIn(['1','2'])
-    @IsNumberString({}, {message: 'La variabel ruta debe ser de tipo Numerico string'})
+    @IsNumberString({}, {message: 'La variable ruta debe ser de tipo Numerico string'})
     ruta: number;
 
     @IsString({message: 'La variable dia debe ser de tipo string'})
