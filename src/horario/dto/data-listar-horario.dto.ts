e0bd6c1013d8a495fca0ed8a27e7978afc1cc650
Jean <jeantorresricse@gmail.com>
2024-03-30 16:12:02 -0500

    modificacion en el dto listar horario

--- src/horario/dto/data-listar-horario.dto.ts
+++ src/horario/dto/data-listar-horario.dto.ts
@@ -2,6 +2,7 @@ import { Type } from "class-transformer";
 import { IsIn, IsInt, IsNumber, IsNumberString, IsString, Min, isIn, } from "class-validator";
 
 export class DataListarHorarioDto{
+
     @Type(() => Number)
     @Min(1, {message: 'La variable id_ruta no debe ser menor a 1'})
     @IsInt({message: 'La variable id_ruta debe ser de tipo numero entero'})
