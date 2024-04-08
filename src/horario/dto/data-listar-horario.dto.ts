9421284894e4d23c13dfb5ad2f720e53d2b257e1
Jean <jeantorresricse@gmail.com>
2024-04-07 19:06:59 -0500

    Se agrego un class validor de opcional en la variable id_ruta y dia en el dto de listar horario

--- src/horario/dto/data-listar-horario.dto.ts
+++ src/horario/dto/data-listar-horario.dto.ts
@@ -6,13 +6,15 @@ export class DataListarHorarioDto{
     @Type(() => Number)
     @Min(1, {message: 'La variable id_ruta no debe ser menor a 1'})
     @IsInt({message: 'La variable id_ruta debe ser de tipo numero entero'})
-    id_ruta: number;
+    @IsOptional()
+    id_ruta?: number;
 
     @IsString({message: 'La variable dia debe ser de tipo string'})
     @IsNotEmpty({message: 'La variable dia no debe estar vacion'})
-    dia: string;
+    @IsOptional()
+    dia?: string;
 
     @IsString({message: 'La variable recorrido debe ser de tipo string'})
-    @IsOptional({message: 'La variable recorrido no debe estar vacio'})
+    @IsOptional()
     recorrido?: string;
 }
