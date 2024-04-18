6f7012d0e917a78cb4f10038000abb17d24f214f
Jean <jeantorresricse@gmail.com>
2024-04-18 18:01:32 -0500

    corrección en el dto de listar personal

--- src/admin_personal/dto/listar-admin_personal.dto.ts
+++ src/admin_personal/dto/listar-admin_personal.dto.ts
@@ -6,8 +6,8 @@ export class ListarAdminPersonal{
 
     @IsOptional()
     @IsNumberString({}, {message: 'La variable numero_carnet debe ser de tipo numerico string'})
-    @MaxLength(11, {message: 'La variable numero carnet debe ser maximo de 11 digitos'})
-    @MinLength(8, {message: 'La variable numero carnet debe ser minimo de 8 digitos'})
+    @MaxLength(11, {message: 'La variable numero_carnet debe ser maximo de 11 digitos'})
+    @MinLength(8, {message: 'La variable numero_carnet debe ser minimo de 8 digitos'})
     numero_carnet?: string;
 
     @IsOptional()
