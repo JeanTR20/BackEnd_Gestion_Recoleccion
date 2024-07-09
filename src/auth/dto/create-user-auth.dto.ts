3527aaafb777cd0f8f26f9ba112f20b7d3ea2b69
Jean <jeantorresricse@gmail.com>
2024-07-09 12:04:41 -0500

    se corregió los class-validator de las variables del dto de crear usuario

--- src/auth/dto/create-user-auth.dto.ts
+++ src/auth/dto/create-user-auth.dto.ts
@@ -12,12 +12,12 @@ export class CreateUserAuhtDto{
     nombre_usuario: string;
 
     @IsString({message: 'La variable telefono debe ser de tipo string'})
-    @IsNotEmpty({message: 'La variable nombre_usuario no debe estar vacio'})
+    @IsNotEmpty({message: 'La variable telefono no debe estar vacio'})
     @Length(9, 9, {message: 'La variable telefono debe tener 9  caracteres'})
     telefono: string;
 
-    @IsString({message: 'La variable nombre_usuario debe ser de tipo string'})
-    @IsNotEmpty({message: 'La variable nombre_usuario no debe estar vacio'})
+    @IsString({message: 'La variable contrasena debe ser de tipo string'})
+    @IsNotEmpty({message: 'La variable contrasena no debe estar vacio'})
     @Length(8, 255, {message: 'La variable contrasena debe tener de 5 a 255 caracteres'})
     contrasena: string;
 
