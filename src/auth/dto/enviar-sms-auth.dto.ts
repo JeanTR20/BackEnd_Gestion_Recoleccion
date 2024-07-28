6c2c996465f5eab6625c8153b7d753373823e6c6
Jean <jeantorresricse@gmail.com>
2024-07-28 18:29:59 -0500

    corrección del la variable telefono del dto del enviar sms

--- src/auth/dto/enviar-sms-auth.dto.ts
+++ src/auth/dto/enviar-sms-auth.dto.ts
@@ -1,7 +1,7 @@
 import { IsNumberString, Length } from "class-validator";
 
 export class EnviarSmsAuthDto{
-    @IsNumberString({}, {message: 'La variable telefono debe ser de tipo number string'})
-    @Length(9, 9, {message: 'La variable telefono deber de de minimo y maximo de 9 digitos'})
+    @IsNumberString({}, {message: 'La campo télefono debe ser de tipo number string'})
+    @Length(9, 9, {message: 'El campo télefono debe tener  minimo y maximo de 9 digitos'})
     telefono: string
 }
