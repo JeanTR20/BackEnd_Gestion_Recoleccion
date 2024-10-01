98e0f86600a7327a44aef8f8e272d112c4e8895f
Jean <jeantorresricse@gmail.com>
2024-10-01 01:45:46 -0500

    se agregó el async en los controladores de las apis de usuario

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -20,10 +20,10 @@ export class UsuarioController {
     description:
       'Esta API permite crear un usuario administrador mediante los parametros:{"id_usuario": "number", "nombre_completo":"string", "apellido_paterno":"string", "apellido_paterno":"string", "apellido_materno":"string","fecha_nacimiento":"string", "direccion":"string", "telefono":"string", "tipo_carnet":"string","carnet_identidad":"string" ,"genero":"string", "imagen":"string", "correo":"string", "nombre_usuario":"string", "contrasena":"string"}. SP: sp_admin_crear_usuario_personal()',
   })
-  crearadministrador(
+  async crearadministrador(
     @Body() crearUsuarioAdministradorDto: CreateUsuarioAdministradorDto
   ){
-    return this.usuarioService.crearUsuarioAdministrador(crearUsuarioAdministradorDto)
+    return await this.usuarioService.crearUsuarioAdministrador(crearUsuarioAdministradorDto)
   }
 
   @Post('crear-personal')
@@ -36,10 +36,10 @@ export class UsuarioController {
     description:
       'Esta API permite crear un usuario personal(recolector) mediante los parametros:{"nombre_completo":"string", "apellido_paterno":"string", "apellido_paterno":"string", "apellido_materno":"string","fecha_nacimiento":"string", "direccion":"string", "telefono":"string", "tipo_carnet":"string","carnet_identidad":"string" ,"genero":"string", "imagen":"string", "correo":"string", "nombre_usuario":"string", "contrasena":"string"}. SP: sp_admin_crear_usuario_personal()',
   })
-  crearusuariopersonal(
+  async crearusuariopersonal(
     @Body() crearUsuarioPersonal:CreateUsuarioPersonal
   ){
-    return this.usuarioService.crearUsuarioPersonal(crearUsuarioPersonal);
+    return await this.usuarioService.crearUsuarioPersonal(crearUsuarioPersonal);
   }
 
   @Patch('actualizar-personal/:id_usuario')
@@ -52,11 +52,11 @@ export class UsuarioController {
     description:
       'Esta API permite actualizar el usuario personal(recolector) mediante los parametros:{"id_usuario":"number", "nombre_completo":"string", "apellido_paterno":"string", "apellido_paterno":"string", "apellido_materno":"string","fecha_nacimiento":"string", "direccion":"string", "telefono":"string", "tipo_carnet":"string","carnet_identidad":"string" ,"genero":"string", "imagen":"string", "correo":"string", "nombre_usuario":"string", "contrasena":"string"}. SP: sp_admin_actualizar_personal()',
   })
-  actualizarPersonal(
+  async actualizarPersonal(
     @Param('id_usuario') id_usuario: number,
     @Body() updateUsuarioPersonalDto: UpdateUsuarioPersonalDto
   ){
-    return this.usuarioService.actualizarPersonal(id_usuario, updateUsuarioPersonalDto)
+    return await this.usuarioService.actualizarPersonal(id_usuario, updateUsuarioPersonalDto)
   }
 
   @Patch('dar-baja-cuenta/:id_usuario')
@@ -69,10 +69,10 @@ export class UsuarioController {
     description:
       'Esta API permite dar de baja a usuario personal(recolector) mediante el parametro:{"id_usuario":"number"}. SP: sp_admin_dar_baja_cuenta()',
   })
-  darbajausuario(
+  async darbajausuario(
     @Param('id_usuario') id_usuario: number 
   ){
-    return this.usuarioService.darBajaUsuario(id_usuario)
+    return await this.usuarioService.darBajaUsuario(id_usuario)
   }
 
   @Patch('activar-cuenta/:id_usuario')
@@ -84,9 +84,9 @@ export class UsuarioController {
     summary: 'Activar cuenta de usuario',
     description: 'Esta API permite activar la cuenta del usuario, mediante el parametro: {"id_usuario":"number"}. SP: sp_admin_activar_cuenta()'
   })
-  activarcuenta(
+  async activarcuenta(
     @Param('id_usuario') id_usuario: number
   ){
-    return this.usuarioService.activarCuenta(id_usuario)
+    return await this.usuarioService.activarCuenta(id_usuario)
   }
 }
